61aac30c110212381074d3831db52b547cb4d753
abhishekgodara <godara.abhishek@gmail.com>
2016-12-16 11:03:09 +0530

    doctor signup process

---
+++ app/src/main/java/com/perraulthealth/Doctorsignupprocessandoptions.java
@@ -0,0 +1,13 @@
+package com.perraulthealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Doctorsignupprocessandoptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctorsignupprocessandoptions);
+    }
+}
